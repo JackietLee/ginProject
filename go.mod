16eea03111faf0a9e4a9daa86007376645a38903
jay <280075924@qq.com>
2023-07-22 17:23:49 +0800

    账号密码

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/gin-gonic/gin v1.7.1
+	gopkg.in/ini.v1 v1.67.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.2
 )
--- model/core.go
+++ model/core.go
@@ -2,15 +2,28 @@ package model
 
 import (
 	"fmt"
+	"gopkg.in/ini.v1"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 var err error
 
 func init() {
-	dsn := "root:root@tcp(127.0.0.1:3306)/springproject?charset=utf8mb4&parseTime=True&loc=Local"
+	cfg, err := ini.Load("./conf/app.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+	ip := cfg.Section("mysql").Key("ip").String()
+	port := cfg.Section("mysql").Key("port").String()
+	user := cfg.Section("mysql").Key("user").String()
+	password := cfg.Section("mysql").Key("password").String()
+	database := cfg.Section("mysql").Key("database").String()
+	//dsn := "root:root@tcp(127.0.0.1:3306)/springproject?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := user + ":" + password + "@tcp(" + ip + ":" + port + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
